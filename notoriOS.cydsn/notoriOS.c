be8e74ae33ab4e2193b00c9e8a7403076c533456
Sharansrj567 <sharansrj567@gmail.com>
2024-10-14 19:15:04 -0400

    commented valve settings

--- notoriOS.cydsn/notoriOS.c
+++ notoriOS.cydsn/notoriOS.c
@@ -66,13 +66,13 @@ void ReadyOrNot()
     
     // Configure server endpoints -- these should obviosuly be provided  remotely by the meta data-base server
     // testing server - travis and meagan june of 2022
-    snprintf(system_settings.ep_host,sizeof(system_settings.ep_host),"%s","ec2-18-222-229-124.us-east-2.compute.amazonaws.com");
+    snprintf(system_settings.ep_host,sizeof(system_settings.ep_host),"%s","lb-stormwater-89156408c3fcbe68.elb.us-east-1.amazonaws.com");
     
     // production server
     //snprintf(system_settings.ep_host,sizeof(system_settings.ep_host),"%s","malcom.open-storm.org");
     
 
-    system_settings.ep_port = 5000;
+    system_settings.ep_port = 4000;
     // Don't need the following for new nodes
     //snprintf(system_settings.ep_user,sizeof(system_settings.ep_user),"%s","generic_node");
     //snprintf(system_settings.ep_pswd,sizeof(system_settings.ep_pswd),"%s","MakeFloodsCurrents");
